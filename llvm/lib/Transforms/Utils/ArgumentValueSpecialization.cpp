344fd791be64383f453db97b408dda9ba220d27b
Keya Malhotra <keya.malhotra@cdol01.cs.ualberta.ca>
2026-07-13 21:20:27 +0000

    [WIP] populate data structures with actual argument+pct

--- llvm/lib/Transforms/Utils/ArgumentValueSpecialization.cpp
+++ llvm/lib/Transforms/Utils/ArgumentValueSpecialization.cpp
@@ -46,7 +46,8 @@ struct CallSiteObservation {
 
 PreservedAnalyses
 ArgumentValueSpecialization::run(Module &M, ModuleAnalysisManager &AM) {
-  std::map<CloneKey, SmallVector<CallSiteObservation, 4>> Candidates;
+  std::map<CloneKey, SmallVector<CallSiteObservation, 4>> IntCandidates;
+  std::map<CloneKey, SmallVector<CallSiteObservation, 4>> FpCandidates;
 
   bool Changed = false;
 
@@ -60,6 +61,8 @@ ArgumentValueSpecialization::run(Module &M, ModuleAnalysisManager &AM) {
         MDNode *IntMD = CB->getMetadata("int-args");
         MDNode *FpMD = CB->getMetadata("fp-args");
 
+
+        // Index for metadata
         unsigned IntPos = 0;
         unsigned FpPos = 0;
 
@@ -69,14 +72,47 @@ ArgumentValueSpecialization::run(Module &M, ModuleAnalysisManager &AM) {
 
         FunctionType *FTy = Callee->getFunctionType();
         unsigned NumArgs = FTy->getNumParams();
+        
 
         for (unsigned ArgIndex = 0; ArgIndex < NumArgs; ++ArgIndex) {
+        
           Type *ValueTy = FTy->getParamType(ArgIndex);
 
-          if (ValueTy->isIntegerTy()) {
-            // handle integer arg
+          if (ValueTy->isIntegerTy() || ValueTy->isPointerTy()) { 
+            if (!IntMD) continue;        
+            
+            auto *ValMeta = cast<ConstantAsMetadata>(IntMD->getOperand(IntPos * 2 + 1));
+            auto *ValConst = cast<ConstantInt>(ValMeta->getValue());
+            uint64_t Value = ValConst->getZExtValue();
+
+            auto *PctMeta = cast<ConstantAsMetadata>(IntMD->getOperand(IntPos * 2 + 2));
+            auto *PctConst = cast<ConstantInt>(PctMeta->getValue());
+            uint8_t Percentage = (uint8_t)PctConst->getZExtValue();
+
+            CloneKey CK{Callee, ArgIndex, Value, std::nullopt};
+            IntCandidates[CK].push_back({CB, Percentage});
+            ++IntPos;
+
           } else if (ValueTy->isFloatingPointTy()) {
-            // handle fp arg
+            if (!FpMD) continue;
+            
+            
+            auto *LoMeta = cast<ConstantAsMetadata>(FpMD->getOperand(FpPos * 3 + 1));
+            auto *LoConst = cast<ConstantInt>(LoMeta->getValue());
+            uint64_t Lo = LoConst->getZExtValue();
+
+            auto *HiMeta = cast<ConstantAsMetadata>(FpMD->getOperand(FpPos * 3 + 2));
+            auto *HiConst = cast<ConstantInt>(HiMeta->getValue());
+            uint64_t Hi = HiConst->getZExtValue();
+
+            auto *PctMeta = cast<ConstantAsMetadata>(FpMD->getOperand(FpPos * 3 + 3));
+            auto *PctConst = cast<ConstantInt>(PctMeta->getValue());
+            uint8_t Percentage = (uint8_t)PctConst->getZExtValue();
+
+            CloneKey CK{Callee, ArgIndex, Lo, Hi};
+            FpCandidates[CK].push_back({CB, Percentage});
+
+            ++FpPos;
           }
         }
       }
